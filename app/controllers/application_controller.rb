6fa72635cf87ebf94e77deff95580b260aa02266
Ryan Burns <ryankburns@gmail.com>
2021-03-09 23:33:41 -0800

    finished user login and created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :require_login
+
+    def current_user
+        if session[:user_id].present?
+            user = User.find_by(:id => session[:user_id])
+        end
+    end
+
+    def require_login
+        unless current_user
+            redirect_to root_url
+        end
+    end
+    
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
 
+    def new
+
+    end
+
+    def create
+        user = User.find_by(:email => params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to '/'
+        else
+            render :new
+        end
+    end
+
     def destroy
         session.clear
         redirect_to '/'
