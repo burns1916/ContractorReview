3ad47ba76d274ac86d3cb92ea5ac09c5bab5cfad
Ryan Burns <ryankburns@gmail.com>
2021-03-09 17:10:59 -0800

    finished client signup

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,26 +1,31 @@
+require 'pry'
 class ClientsController < ApplicationController
 
     def new
         @client = Client.new
+        @client.build_user
     end
 
     def create
-        raise params.inspect
+
         @client = Client.new(client_params)
+
             if @client.save
-                redirect_to :show
+                @client.user.meta = @client
+       
+                redirect_to @client
             else
                 render :new
             end
     end
 
     def show
-
+        @client = Client.find_by_id(params[:id])
     end
 
     private
 
     def client_params
-        params.require(:client).permit(:name, user_attributes: [:id, :email, :password])
+        params.require(:client).permit(:name, { user_attributes: [:id, :email, :password] } )
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     belongs_to :meta, polymorphic: true
+    has_secure_password
 end
