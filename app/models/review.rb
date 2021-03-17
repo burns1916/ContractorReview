26dd140fe5af9694b59769fcf08d3f4f68638691
Ryan Burns <ryankburns@gmail.com>
2021-03-16 21:56:52 -0700

    modified views

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
     belongs_to :appointment
 
     validates :rating, presence: true
+    validates :rating, numericality: { less_than_or_equal_to: 10 }
     validates :content, presence: true
 end
